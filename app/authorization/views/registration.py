75bcdb43cd390d0c3d9ad88a89e3ea0b560e508f
Atryom Khabibyllin <238136@mail.ru>
2022-08-04 11:43:56 +0300

    Update registration.py

--- app/authorization/views/registration.py
+++ app/authorization/views/registration.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.models import User
 from drf_spectacular.utils import extend_schema, inline_serializer
 from rest_framework import status, serializers
@@ -11,44 +10,42 @@ from authorization.serializers import UserSerializer
 from avia.api.schemas import response_400
 
 
-class RegistrationView(APIView):
-    authentication_classses = []
-    
+class RegistrationVIew(APIView):
+    authentication_classes = []
+
     @extend_schema(
-      summary='Регистрация пользователя',
-      description='Регистрация пользователей в системе',
-      tags=['Authorization'],
-      request=inline_serializer(
+        summary="Регистрация",
+        description="Регистрация пользователя в системе",
+        tags=["Authorization"],
+        request=inline_serializer(
             name='AuthSerializer',
             fields={
                 'username': serializers.CharField(),
                 'email': serializers.EmailField(),
-                'password': serializers.CharField()
+                'password': serializers.CharField(),
             },
-      ),
-      responses={
-        201: auth_response,
-        400: response_400,
-      }
+        ),
+        responses={
+            201: auth_response,
+            400: response_400,
+        }
     )
     def post(self, request):
-      serializer = UserSerializer(data=request.data)
-      serializer.is_valid(raise_exeption=True)
-      user = serializer.validated_data
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
 
-      user = User.objects.create_user(
-        username=user.get('username'),
-        email=user.get('email'),
-        password=user.get('password'),
-      )
+        user = User.objects.create_user(
+            username=user.get('username'),
+            email=user.get('email'),
+            password=user.get('password'),
+        )
 
-      refresh = RefreshToken.for_user(user)
-      data = {
-        'refresh': str(refresh),
-        'access': str(refrest.access_token)
-      }
+        refresh = RefreshToken.for_user(user)
 
-      return Response(data=data, status=status.HTTP_201_CREATED)
+        data = {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
 
-
-
+        return Response(data=data, status=status.HTTP_201_CREATED)
